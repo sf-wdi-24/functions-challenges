aa64698491842792ac1415f1e958553de58dcc57
murcielago17 <murcielago17@hotmail.com>
2015-10-28 17:08:08 -0700

    define sillySum function for loop array

---
+++ functionsChallenges.js
@@ -0,0 +1,30 @@
+function maxOrMin(max, x, y) {
+    if (max && x > y) {
+        return x
+    } else if (max && y > x) {
+        return y
+    }
+}
+alert(maxOrMin(true,9,7))
+
+
+
+
+
+
+var arr = [1,4,6,8,9,7]
+function sillySum (array) {
+	var newArr =[]; 
+	for (var i = 0; i < array.length; i++) {
+		
+		 newArr.push(i*array[i]); 
+
+	}
+	return newArr }
+
+sillySum(arr)
+[0, 4, 12, 24, 36, 35]
+
+
+
+
